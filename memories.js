e1b6c5211e7b76c159a5fe27567d8a2e76a36521
tengukid <duffykjc@gmail.com>
2025-02-21 14:40:21 +0000

    added memory page
    add memory page

--- memories.js
+++ memories.js
@@ -2,3 +2,36 @@ function darkToggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+window.onscroll = function () {
+
+    const nav = document.getElementById("mainNav");
+
+
+    if (window.scrollY > 60) {
+
+        nav.classList.add("scrolled")
+    }
+
+    else {
+
+        nav.classList.remove("scrolled")
+    }
+
+
+}
+
+const input = document.querySelector("input");
+const form = document.querySelector("#memoriesform");
+const ul = document.querySelector("ul");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    li.textContent = input.value;
+    input.value = "";
+});
+
+
